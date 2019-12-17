6ed6030c774bea8c35cdd37107ab1c2c320e4135
Teodor Stoyanov <teo.stoyanov1@gmail.com>
2019-12-17 10:33:37 +0200

    Remove wild card in ToDoList class

--- src/main/java/primeholding/entities/ToDoList.java
+++ src/main/java/primeholding/entities/ToDoList.java
@@ -5,7 +5,19 @@ import lombok.Data;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 
